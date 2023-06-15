848aacd3ec8232afce137476d9a466dffa2c77f9
Hưng Việt <viethung20101@gmail.com>
2023-06-15 16:49:41 +0700

    Master page 1/2

---
+++ Nhom6_IS385L/MasterPage.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Nhom6_IS385L
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        KetNoi kn = new KetNoi();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false) 
+            {
+                this.DataList1.DataSource = kn.LayDuLieu("SELECT * FROM ");
+                this.DataList1.DataBind();
+            }
+        }
+    }
+}
